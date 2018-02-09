827e7e3ad9f933dfcfb414fb1bd434c3cc1f955d
wlingxiao <wlingxiao@outlook.com>
2018-02-09 22:26:29 +0800

    more example

--- src/test/scala/hikari/EmbeddedChannelHttpExample.scala
+++ src/test/scala/hikari/EmbeddedChannelHttpExample.scala
@@ -1,31 +1,14 @@
 package hikari
 
-import java.nio.charset.{Charset, StandardCharsets}
-
-import example.HttpHelloWorldServerHandler
-import hikari.InternalRoute._
-import io.netty.buffer.{ByteBuf, Unpooled}
-import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
+import io.netty.buffer.Unpooled
 import io.netty.channel.embedded.EmbeddedChannel
 import io.netty.handler.codec.http._
-import io.netty.handler.codec.string.StringDecoder
-import io.netty.handler.codec.{DelimiterBasedFrameDecoder, Delimiters}
 import io.netty.util.CharsetUtil
 
-class HelloWorldHandler extends SimpleChannelInboundHandler[FullHttpRequest] {
-
-  override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
-    ctx.flush()
-  }
-
-  override def channelRead0(ctx: ChannelHandlerContext, httpRequest: FullHttpRequest): Unit = {
-    println("hello world")
-  }
-
-
-}
-
-object HikariExample extends App {
+/**
+  * https://searchcode.com/codesearch/view/25262543/
+  */
+object EmbeddedChannelHttpExample {
 
   val channel = new EmbeddedChannel(new HttpRequestDecoder())
   val p = channel.pipeline()
