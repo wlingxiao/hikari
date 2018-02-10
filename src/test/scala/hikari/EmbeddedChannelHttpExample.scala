6218a35404a04b0619860264724d7c09a4a46cbd
wlingxiao <wlingxiao@outlook.com>
2018-02-10 10:23:47 +0800

    more example

--- src/test/scala/hikari/EmbeddedChannelHttpExample.scala
+++ src/test/scala/hikari/EmbeddedChannelHttpExample.scala
@@ -7,15 +7,11 @@ import io.netty.util.CharsetUtil
 
 /**
   * https://searchcode.com/codesearch/view/25262543/
+  * https://github.com/zuoyanyouwu/netty-handler-tester
   */
-object EmbeddedChannelHttpExample {
+object EmbeddedChannelHttpExample extends App {
 
-  val channel = new EmbeddedChannel(new HttpRequestDecoder())
-  val p = channel.pipeline()
-  p.addLast(new HttpServerCodec())
-  p.addLast(new HttpObjectAggregator(Short.MaxValue)) // 必不可少
-  p.addLast(new HttpServerExpectContinueHandler())
-  p.addLast(new BasicHandler)
+  val channel = new EmbeddedChannel(new HttpRequestDecoder(), new HttpObjectAggregator(Short.MaxValue), new BasicHandler)
   val crlf = "\r\n"
 
   val request = "GET /some/path HTTP/1.1" + crlf +
@@ -25,6 +21,7 @@ object EmbeddedChannelHttpExample {
     "MyTestHeader2: part21" + crlf +
     "\t            newLinePart22" + crlf + crlf
   channel.writeInbound(Unpooled.copiedBuffer(request, CharsetUtil.US_ASCII))
-  val req: HttpRequest = channel.readInbound()
+  val req: FullHttpResponse = channel.readOutbound[FullHttpResponse]()
+  println(req)
 
 }
--- src/test/scala/hikari/HikariExample.scala
+++ src/test/scala/hikari/HikariExample.scala
@@ -1,36 +1,29 @@
 package hikari
 
-import io.netty.buffer.Unpooled
-import io.netty.channel.embedded.EmbeddedChannel
-import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
-import io.netty.handler.codec.http._
-import io.netty.util.CharsetUtil
-
-class HelloWorldHandler extends SimpleChannelInboundHandler[FullHttpRequest] {
-
-  override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
-    ctx.flush()
-  }
-
-  override def channelRead0(ctx: ChannelHandlerContext, httpRequest: FullHttpRequest): Unit = {
-    println("hello world")
-  }
-
-
-}
+import hikari.InternalRoute._
 
 object HikariExample extends App {
 
-  val channel = new EmbeddedChannel(new HttpRequestDecoder(), new HttpObjectAggregator(Short.MaxValue), new BasicHandler)
-  val crlf = "\r\n"
+  before("/users/*") { (request, response) =>
 
-  val request = "GET /some/path HTTP/1.1" + crlf +
-    "Host: localhost" + crlf +
-    "MyTestHeader: part1" + crlf +
-    "              newLinePart2" + crlf +
-    "MyTestHeader2: part21" + crlf +
-    "\t            newLinePart22" + crlf + crlf
-  channel.writeInbound(Unpooled.copiedBuffer(request, CharsetUtil.US_ASCII))
-  val req: FullHttpResponse = channel.readOutbound[FullHttpResponse]()
-  println(req)
+    println("before users")
+
+  }
+
+  get("/users/:id") { (request, response) =>
+
+    println(request.cookies())
+
+    println(request.cookie("name"))
+
+    request.pathParam("id")
+
+    throw new UnsupportedOperationException
+  }
+
+  after("/users/*") { (request, response) =>
+    println("after users")
+  }
+
+  HikariServer.start()
 }
