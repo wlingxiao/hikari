327779371d31189188d315c07c08c2990ed33103
wlingxiao <wlingxiao@outlook.com>
2018-02-10 10:06:15 +0800

    完善响应 Example

--- src/test/scala/hikari/HikariExample.scala
+++ src/test/scala/hikari/HikariExample.scala
@@ -1,15 +1,9 @@
 package hikari
 
-import java.nio.charset.{Charset, StandardCharsets}
-
-import example.HttpHelloWorldServerHandler
-import hikari.InternalRoute._
-import io.netty.buffer.{ByteBuf, Unpooled}
-import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
+import io.netty.buffer.Unpooled
 import io.netty.channel.embedded.EmbeddedChannel
+import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
 import io.netty.handler.codec.http._
-import io.netty.handler.codec.string.StringDecoder
-import io.netty.handler.codec.{DelimiterBasedFrameDecoder, Delimiters}
 import io.netty.util.CharsetUtil
 
 class HelloWorldHandler extends SimpleChannelInboundHandler[FullHttpRequest] {
@@ -27,12 +21,7 @@ class HelloWorldHandler extends SimpleChannelInboundHandler[FullHttpRequest] {
 
 object HikariExample extends App {
 
-  val channel = new EmbeddedChannel(new HttpRequestDecoder())
-  val p = channel.pipeline()
-  p.addLast(new HttpServerCodec())
-  p.addLast(new HttpObjectAggregator(Short.MaxValue)) // 必不可少
-  p.addLast(new HttpServerExpectContinueHandler())
-  p.addLast(new BasicHandler)
+  val channel = new EmbeddedChannel(new HttpRequestDecoder(), new HttpObjectAggregator(Short.MaxValue), new BasicHandler)
   val crlf = "\r\n"
 
   val request = "GET /some/path HTTP/1.1" + crlf +
@@ -42,6 +31,6 @@ object HikariExample extends App {
     "MyTestHeader2: part21" + crlf +
     "\t            newLinePart22" + crlf + crlf
   channel.writeInbound(Unpooled.copiedBuffer(request, CharsetUtil.US_ASCII))
-  val req: HttpRequest = channel.readInbound()
-
+  val req: FullHttpResponse = channel.readOutbound[FullHttpResponse]()
+  println(req)
 }
